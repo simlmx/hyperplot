1b75130de2fe8770148d49155487e1b348391379
Simon Lemieux <simon@datacratic.com>
2013-01-03 12:02:27 -0500

    added setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='Hyperplot',
+      version='0.1',
+      description='Database filtering and plotting facilities.',
+      author='Simon Lemieux',
+      author_email='lemieux.simon@gmail.com',
+      url='github.com/simlmx/hyperplot',
+      packages=['hyperplot'],
+)
